8b5e9b162dc9ea6e0d541e24b4a43e872034a27d
$HIFU <104694378+SHIFU851@users.noreply.github.com>
2024-09-29 17:40:38 +0300

    Delete Info making list/ip_info_maker directory

--- Info making list/ip_info_maker/socialmedia.py
+++
@@ -1,5 +0,0 @@
-socialmedia = [
-    "Facebook", "Instagram", "Twitter (X)", "Snapchat",
-    "TikTok", "Reddit", "LinkedIn", "Pinterest", "YouTube",
-    "WhatsApp", "Telegram", "WeChat", "Viber", "Signal" "Github"
-]
