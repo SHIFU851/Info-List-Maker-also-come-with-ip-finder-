c0a74f7a577f4de1a6d834541871623e8616a00e
$HIFU <104694378+SHIFU851@users.noreply.github.com>
2024-09-29 17:40:57 +0300

    Add files via upload

---
+++ Fixing.py
@@ -0,0 +1,7 @@
+import traceback
+try:
+    # Your code here
+    pass  # Replace with actual code
+except Exception as e:
+    print("An error occurred:")
+    traceback.print_exc()  # This will print the full traceback
---
+++ socialmedia.py
@@ -0,0 +1,5 @@
+socialmedia = [
+    "Facebook", "Instagram", "Twitter (X)", "Snapchat",
+    "TikTok", "Reddit", "LinkedIn", "Pinterest", "YouTube",
+    "WhatsApp", "Telegram", "WeChat", "Viber", "Signal" "Github"
+]
